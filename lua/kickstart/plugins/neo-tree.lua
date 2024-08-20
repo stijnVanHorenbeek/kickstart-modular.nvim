5a9cd4e10d4c2c9acad77660e80fb5414b9424be
stijnVanHorenbeek <stijn.van.horenbeek@outlook.com>
2024-08-20 12:02:06 +0200

    update neo-tree config

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -14,7 +14,19 @@ return {
     { '<leader>e', ':Neotree toggle<CR>', desc = 'NeoTree toggle' },
   },
   opts = {
+    close_if_last_window = true,
     filesystem = {
+      filtered_items = {
+        visible = false,
+        hide_dotfiles = false,
+        hide_gitignore = false,
+      },
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = false,
+      },
+      hijack_netrw_behavior = 'open_default',
+      use_libuv_file_watcher = true,
       window = {
         mappings = {
           ['<leader>e'] = 'close_window',
